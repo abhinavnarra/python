bd2a8c13571664d1ecc56a8087a79ed2d00c1141
abhinavnarra <38247637+abhinavnarra@users.noreply.github.com>
2018-04-25 14:22:10 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,30 @@
+#Demonstrate python program to copy files and by using crontab it need to be automate.
+import shutil
+from shutil import copyfile
+
+
+shutil.copyfile('file1.txt', 'file2.txt')
+
+
+
+
+#by using crontab it can be automated in following way
+
+
+
+#Make the script executable by:
+
+#chmod u+x C:/CODING TEST/p6.py
+
+#setting up executable python environment
+#!/usr/bin/env python3
+
+
+#Open your cron table by
+#crontab -e 
+
+#Add the following cron entry:
+#*/10 * * * *   C:/CODING TEST/p6.py#running cronjob for every 10 minutes
+
+
+
