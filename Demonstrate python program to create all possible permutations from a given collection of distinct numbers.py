225fe32a9d50889ad592f81ab0b984272dddd929
abhinavnarra <38247637+abhinavnarra@users.noreply.github.com>
2018-04-26 14:20:12 +0530

    Add files via upload

---
+++ Demonstrate python program to create all possible permutations from a given collection of distinct numbers.py
@@ -0,0 +1,18 @@
+#Demonstrate python program to create all possible permutations from a given collection of distinct numbers
+
+def permute(nums):
+  if nums == []:
+    return [[]]
+  permutations=[]
+  for i,n in enumerate(nums):
+    other_permutations=permute(nums[:i] + nums[i+1:])
+    permutations+=map(lambda permutation: [n] + permutation,other_permutations)
+  return permutations
+print(permute([1,2,3]))
+
+#this can also be done by importing itertools
+import itertools
+lst = [1,2,3]
+print(set(itertools.permutations(lst)))
+
+
